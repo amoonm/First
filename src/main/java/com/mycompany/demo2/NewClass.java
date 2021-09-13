c86108ec3fcd13461ba08efba20809d46f819ec8
Amoon Mainali <amoonmainali8@gmail.com>
2021-09-13 10:25:25 -0400

    Add array object with initial and access

--- src/main/java/com/mycompany/demo2/NewClass.java
+++ src/main/java/com/mycompany/demo2/NewClass.java
@@ -11,13 +11,15 @@ package com.mycompany.demo2;
  */
 public class NewClass {
     public static void main(String[] args) {
-        int x=0, y=10;
+        int [] arr = new int[5];
         
-        do{
-            x++;
-            System.out.println("i = " +x );
-            
-        }while(x!=0);
+        for(int i=0; i< arr.length; i++) {
+            arr [i] = 10 + i;
+        }
+        
+        for(int i=0; i< arr.length; i++) {
+            System.out.println("Arr["+i+"]: " + arr [i]);
+        }
         
         
     }
