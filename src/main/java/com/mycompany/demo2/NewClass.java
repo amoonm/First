5466a4730d4b13146d0ad120303ea61cdc14ab9d
Amoon Mainali <amoonmainali8@gmail.com>
2021-09-13 09:51:30 -0400

    Initial Commit - project created

---
+++ src/main/java/com/mycompany/demo2/NewClass.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.demo2;
+
+/**
+ *
+ * @author amoonmainali
+ */
+public class NewClass {
+    public static void main(String[] args) {
+        int x=0, y=10;
+        
+        do{
+            x++;
+            System.out.println("i = " +x );
+            
+        }while(x!=0);
+        
+        
+    }
+    
+}
